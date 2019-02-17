3f65e19824d31ce546d797c51116607a23f79d73
Viktor Orda <ordavictor@gmail.com>
2019-02-18 00:35:07 +0200

    Add solution to exercise1

---
+++ lesson7/Queue-two_stacks.php
@@ -0,0 +1,61 @@
+<?php
+
+class Stack
+{
+    private $storage = [];
+    private $top = 0;
+
+    public function add($value)
+    {
+        $this->storage[$this->top++] = $value;
+    }
+
+    public function isEmpty()
+    {
+        return $this->top === 0;
+    }
+
+    public function getLast()
+    {
+        if ($this->isEmpty()) {
+            exit('Stack is empty!');
+        }
+        return $this->storage[$this->top - 1];
+    }
+
+    public function remove()
+    {
+        if ($this->isEmpty()) {
+            exit('Stack is empty!');
+        }
+        return $this->storage[--$this->top];
+    }
+}
+
+class Queue
+{
+    private $stack1, $stack2;
+
+    public function __construct()
+    {
+        $this->stack1 = new Stack(); 
+        $this->stack2 = new Stack();
+    }
+
+    public function add($value)
+    {
+        $this->stack1->add($value);
+    }
+
+    public function remove()
+    {
+        if ($this->stack1->isEmpty() && $this->stack2->isEmpty()) {
+            exit('Queue is empty!');
+        } elseif ($this->stack2->isEmpty()) {
+            while (!$this->stack1->isEmpty()) {
+                $this->stack2->add($this->stack1->remove());
+            }
+        }
+        return $this->stack2->remove();
+    }
+}
